f3eae590963b805d985eb6a4853bd10813b6379c
jirawat070 <jirawat_pao@hotmail.co.th>
2018-10-20 18:40:56 +0700

    add comfirm alert

--- server.js
+++ server.js
@@ -99,6 +99,7 @@ app.get('/product_delete/:pid',function (req, res) {
     db.any(sql)
         .then(function(data){
             console.log('DATA:'+data);
+            
             res.render('pages/products',{products : data});
             
         })
